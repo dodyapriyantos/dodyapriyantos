25962c9d2fd5b45a281d127a64b9037e310d39f0
dodyapriyantos <dody.apriyanto@gmail.com>
2022-04-29 08:36:32 +0700

    Create practice_numpy.py

---
+++ Practice_Numpy/practice_numpy.py
@@ -0,0 +1,19 @@
+import numpy
+
+
+#fill all vector to empty or zeros
+NumpyPracticeEmpty = numpy.empty(10)
+print NumpyPracticeEmpty
+
+#fill all vector to zeros
+NumpyPracticeZeros = numpy.zeros(11)
+print NumpyPracticeZeros
+
+#fill all vector to empty or ones
+NumpyPracticeOnes = numpy.ones(12)
+print NumpyPracticeOnes
+
+#create array and fill it
+NumpyPracticeArray = numpy.array([0,1,2,3,4,5,6,7,8,9])
+print NumpyPracticeArray
+print NumpyPracticeArray[2]
